47fe740874ff916d757d0f21e2de5c919f80711d
Mouhamad <mouhamad@192.168.0.121>
2024-07-12 18:00:20 +0000

    feat : improve header disigne

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {path : "", component : LoginComponent},
   {path : "login", component : LoginComponent},
-
+  {path : "home", component : HomeComponent},
+  {path : "dashboard", component : DashboardComponent},
 
   {path : "admin", component : AdminComponent, canActivate : [AuthGuard], children : [
     {path : "home", component : HomeComponent},
